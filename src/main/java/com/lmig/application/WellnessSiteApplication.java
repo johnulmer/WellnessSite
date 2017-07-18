94b92317cddaf5bdc2e8263857efae07634c606b
JebCastelo <jeb.castelo@libertymutual.com>
2017-07-18 12:16:23 -0400

    Commit 1

--- src/main/java/com/lmig/application/WellnessSiteApplication.java
+++ src/main/java/com/lmig/application/WellnessSiteApplication.java
@@ -9,8 +9,10 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class WellnessSiteApplication {
 
 	public static void main(String[] args) {
