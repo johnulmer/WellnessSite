05a1b5e3d2883d7104aef468ee074790b35504b1
john ulmer <john.ulmer@libertymutual.com>
2017-07-26 16:38:20 -0400

    additional activity work done and demo data rebuild option

--- src/main/java/com/lmig/application/activityImplementations/MeetingGoalsActivityImplementation.java
+++ src/main/java/com/lmig/application/activityImplementations/MeetingGoalsActivityImplementation.java
@@ -50,6 +50,69 @@ public class MeetingGoalsActivityImplementation {
 		}
 		return returnList;
 	}
+	
+	public ArrayList<Integer> getEventList() {
+		ArrayList<Integer> returnList = new ArrayList<Integer>();
+		String sql = "Select distinct \"EVENT_ID\" from public.\"MEETING_GOALS\"";
+		try (Connection conn = this.connect(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				returnList.add(rs.getInt("EVENT_ID"));
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return returnList;
+	}
+	
+	public void reset() {
+		String dropTableSQL = "DROP TABLE IF EXISTS public.\"MEETING_GOALS\"";
+		String createTableSQL = "CREATE TABLE public.\"MEETING_GOALS\"\r\n" + 
+				"(\r\n" + 
+				"    \"EVENT_ID\" integer,\r\n" + 
+				"    \"MEMBER_ID\" integer,\r\n" + 
+				"    \"ACTIVITY_DATE\" date,\r\n" + 
+				"    \"GOAL\" double precision,\r\n" + 
+				"    \"ACTUAL\" double precision\r\n" + 
+				")\r\n" + 
+				"WITH (\r\n" + 
+				"    OIDS = FALSE\r\n" + 
+				")\r\n" + 
+				"TABLESPACE pg_default";
+
+		try (Connection conn = this.connect(); 
+				PreparedStatement pstmtDrop = conn.prepareStatement(dropTableSQL);
+				PreparedStatement pstmtCreate = conn.prepareStatement(createTableSQL);) {
+			pstmtDrop.execute();
+			pstmtCreate.execute();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		resetRows();
+	}
+	
+	private void resetRows() {
+		String updateRowsSQL = "INSERT INTO public.\"MEETING_GOALS\" (\"EVENT_ID\", \"MEMBER_ID\", \"ACTIVITY_DATE\", \"ACTUAL\", \"GOAL\")\r\n" + 
+				"VALUES\r\n" + 
+				"(1, 1, '2017-06-20', 70, 64),\r\n" + 
+				"(1, 2, '2017-06-20', 130, 128),\r\n" + 
+				"(1, 3, '2017-06-20', 93, 96),\r\n" + 
+				"(1, 1, '2017-06-21', 64, 64),\r\n" + 
+				"(1, 2, '2017-06-21', 128, 128),\r\n" + 
+				"(1, 3, '2017-06-21', 99, 128),\r\n" + 
+				"(8, 1, '2017-07-20', 112, 128),\r\n" + 
+				"(8, 2, '2017-07-20', 132, 128),\r\n" + 
+				"(8, 3, '2017-07-20', 100, 128),\r\n" + 
+				"(8, 1, '2017-07-21', 94, 128),\r\n" + 
+				"(8, 2, '2017-07-21', 128, 128),\r\n" + 
+				"(8, 3, '2017-07-21', 111, 128)";
+		try (Connection conn = this.connect(); 
+				PreparedStatement pstmtUpdateRows = conn.prepareStatement(updateRowsSQL);) {
+			pstmtUpdateRows.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 
 public Connection connect() {
 	// SQLite connection string
--- src/main/java/com/lmig/application/activityImplementations/PerformanceImprovementActivityImplementation.java
+++ src/main/java/com/lmig/application/activityImplementations/PerformanceImprovementActivityImplementation.java
@@ -14,7 +14,7 @@ public class PerformanceImprovementActivityImplementation {
 
 	public ArrayList<PerformanceImprovementActivityRow> getEventRows(int eventID) {
 		ArrayList<PerformanceImprovementActivityRow> returnList = new ArrayList<PerformanceImprovementActivityRow>();
-		String sql = "Select * from public.\"MEETING_GOALS\" WHERE \"EVENT_ID\" = ?";
+		String sql = "Select * from public.\"PERFORMANCE_IMPROVEMENT\" WHERE \"EVENT_ID\" = ?";
 		try (Connection conn = this.connect(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
 			pstmt.setInt(1, eventID);
 			ResultSet rs = pstmt.executeQuery();
@@ -31,6 +31,63 @@ public class PerformanceImprovementActivityImplementation {
 		return returnList;
 	}
 
+	public ArrayList<Integer> getEventList() {
+		ArrayList<Integer> returnList = new ArrayList<Integer>();
+		String sql = "Select distinct \"EVENT_ID\" from public.\"PERFORMANCE_IMPROVEMENT\"";
+		try (Connection conn = this.connect(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				returnList.add(rs.getInt("EVENT_ID"));
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return returnList;
+	}
+	
+	public void reset() {
+		String dropTableSQL = "DROP TABLE IF EXISTS public.\"PERFORMANCE_IMPROVEMENT\"";
+		String createTableSQL = "CREATE TABLE public.\"PERFORMANCE_IMPROVEMENT\"\r\n" + 
+				"(\r\n" + 
+				"    \"EVENT_ID\" integer NOT NULL,\r\n" + 
+				"    \"MEMBER_ID\" integer NOT NULL,\r\n" + 
+				"    \"START_STAT\" double precision NOT NULL,\r\n" + 
+				"    \"END_STAT\" double precision NOT NULL\r\n" + 
+				")\r\n" + 
+				"WITH (\r\n" + 
+				"    OIDS = FALSE\r\n" + 
+				")\r\n" + 
+				"TABLESPACE pg_default";
+
+		try (Connection conn = this.connect(); 
+				PreparedStatement pstmtDrop = conn.prepareStatement(dropTableSQL);
+				PreparedStatement pstmtCreate = conn.prepareStatement(createTableSQL);) {
+			pstmtDrop.execute();
+			pstmtCreate.execute();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		resetRows();
+	}
+	
+	private void resetRows() {
+		String updateRowsSQL = "INSERT INTO public.\"PERFORMANCE_IMPROVEMENT\" \r\n" + 
+				"(\"EVENT_ID\", \"MEMBER_ID\", \"START_STAT\", \"END_STAT\")\r\n" + 
+				"VALUES\r\n" + 
+				"(3, 1, 7.5, 5.0),\r\n" + 
+				"(3, 2, 8.5, 4.25),\r\n" + 
+				"(3, 3, 4.2, 3.1),\r\n" + 
+				"(6, 1, 6.2, 5.4),\r\n" + 
+				"(6, 2, 7.5, 6.25),\r\n" + 
+				"(6, 3, 8.2, 7.1)";
+		try (Connection conn = this.connect(); 
+				PreparedStatement pstmtUpdateRows = conn.prepareStatement(updateRowsSQL);) {
+			pstmtUpdateRows.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
 public Connection connect() {
 	// SQLite connection string
 	String url = WellnessSiteDB.DBURL;
