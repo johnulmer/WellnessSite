ada448d1ad71c08bdc80561b1d6503c51fd64e3a
JebCastelo <jeb.castelo@libertymutual.com>
2017-08-01 11:49:12 -0400

    fixed 10k and 5k dates same day

--- src/main/java/com/lmig/application/restControllers/WellnessEventController.java
+++ src/main/java/com/lmig/application/restControllers/WellnessEventController.java
@@ -145,17 +145,17 @@ public class WellnessEventController implements Controller {
 		LocalDate endTres = LocalDate.now().plusWeeks(3);
 		LocalDate endTreso = LocalDate.now().plusWeeks(4);
 
-		wellnessEventRepo.save(new WellnessEvent("Joe's Challenge", "Barrington", "Just a 5K", "5K", startUno, endUno,
+		wellnessEventRepo.save(new WellnessEvent("Joe's Challenge", "Barrington", "Just a 5K", "5K", startUno, startUno,
 				"14 Hunter Lane", "Barrington", "NH", "03825", 43.191904, -71.019577));
-		wellnessEventRepo.save(new WellnessEvent("Jeb's Challenge", "Fishers", "A 10K of fun", "10K", startDos, endDos,
+		wellnessEventRepo.save(new WellnessEvent("Jeb's Challenge", "Fishers", "A 10K of fun", "10K", startDos, startDos,
 				"14277 Camelot House Way", "Fishers", "IN", "46037", 39.980659, -85.909026));
-		wellnessEventRepo.save(new WellnessEvent("Dave's Challenge", "GreenLand", "Dave's Monthly Challenge", "Step Count",startDos, endDos, 
+		wellnessEventRepo.save(new WellnessEvent("Dave's Challenge", "GreenLand", "Dave's Monthly Challenge", "Step Count",startTres, startTres, 
 				"60 Mc Shane Avenue", "GreenLand", "NH", "03840", 43.050105, -70.829194));		
 		wellnessEventRepo.save(new WellnessEvent("John's Challenge", "New York", "Step Challenge", "Step Count",startTres, endTres, 
 				"725 5th Ave", "New York", "NY", "10022", 40.762374, -73.973912));
-		wellnessEventRepo.save(new WellnessEvent("Lucas 10K", "Somwersworth", "Walks on the beach", "10k", startTreso,endTreso,
+		wellnessEventRepo.save(new WellnessEvent("Lucas 10K", "Somwersworth", "Walks on the beach", "10k", startTreso, startTreso,
 				"16 Cameron Way", "Somersworth", "NH", "03878", 43.282887, -70.889938));
-		wellnessEventRepo.save(new WellnessEvent("Liberty Event", "Dover", "Romantic Run", "5k", startTres, endTres,
+		wellnessEventRepo.save(new WellnessEvent("Liberty Event", "Dover", "Romantic Run", "5k", endTres, endTres,
 				"150 Liberty Way", "Dover", "NH", "03820", 43.209739, -70.912609));
 		wellnessEventRepo.save(new WellnessEvent("Ron's Challenge", "Wausau", "Steps Up", "Step Count", startTreso, endTreso,
 				"1305 N 14th St", "Wausau", "WI", "54403", 44.968023, -89.608543));
@@ -165,17 +165,17 @@ public class WellnessEventController implements Controller {
 				"157 Berkeley Street", "Boston", "MA", "02116", 42.349154, -71.071818));
 		wellnessEventRepo.save(new WellnessEvent("Liberty Corporate 2nd Round", "Boston", "Step Fun", "StepCount", startTreso, endTreso,
 				"157 Berkeley Street", "Boston", "MA", "02116", 42.349154, -71.071818));
-		wellnessEventRepo.save(new WellnessEvent("Rural Run", "Hatford City", "5K Run", "5K", startUno, endDoso,
+		wellnessEventRepo.save(new WellnessEvent("Rural Run", "Hatford City", "5K Run", "5K", endDoso, endDoso,
 				"809 North High Street", "Hartford City", "IN", "47348", 40.457519, -85.368778));
-		wellnessEventRepo.save(new WellnessEvent("White House Run", "Washhington DC", "10K Run", "10K", startUnos, endDoso,
+		wellnessEventRepo.save(new WellnessEvent("White House Run", "Washhington DC", "10K Run", "10K", startUnos, startUnos,
 				"1600 Pennsylvania Avenue ", "Hartford City", "IN", "20006", 38.897663, -77.036574));
-		wellnessEventRepo.save(new WellnessEvent("Platform 24", "Carmel", "Open Space Run", "10K", startUno, endUnos,
+		wellnessEventRepo.save(new WellnessEvent("Platform 24", "Carmel", "Open Space Run", "10K", startTres, startTres,
 				"12400 N Meridian Street", "Carmel", "IN", "46032", 39.9705529, -86.1596646));
 		wellnessEventRepo.save(new WellnessEvent("Gillette Stadium", "Foxborough", "Fan Challenge", "StepCount", startDos, endDos,
 				"1 Patriot Place", "Foxborough", "MA", "02035", 42.091188, -71.26688899999999));
 		wellnessEventRepo.save(new WellnessEvent("Safeco Mountan Run", "Seattle", "Mt. Rainer", "StepCount", startDoso, endTreso,
 				"1001 4th Ave", "Seattle", "WA", "98154", 47.6061304, -122.33384940000002));
-		wellnessEventRepo.save(new WellnessEvent("Run with Elvis", "Memphis", "Loose some weight 70's Elvis", "10K", startDoso, endTreso,
+		wellnessEventRepo.save(new WellnessEvent("Run with Elvis", "Memphis", "Loose some weight 70's Elvis", "10K", endUno, endUno,
 				"Elvis Presley Blvd", "Memphis", "TN", "38116", 35.0302462, -90.02517030000001));
 		wellnessEventRepo.save(new WellnessEvent("Run with Prince", "Chanhassen", "Step it out with Prince", "Step Count", startUno, endTreso,
 				"7801 Audubon Rd", "Chanhassen", "MN", "55317", 44.8617041, -93.5605883));
